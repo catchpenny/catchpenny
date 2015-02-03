403ff80c7024145400c853473c1336de51210de5
tahaa karim <tahaa_libra@hotmail.com>
2015-02-03 16:19:31 +0530

    Added Appropriate message to homepage

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,9 +5,7 @@ class HomeController extends Controller
 
     public function home()
     {   
-        echo "U Have Arrived!! Now You May Leave";
-
-
+       
     }
 
 
--- app/views/sentient/home.php
+++ app/views/sentient/home.php
@@ -1,1 +1,10 @@
-yoyo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CatchPenny Project</title>
+</head>
+<body>
+    Welcome to CatchPenny Project. 
+</body>
+</html>
