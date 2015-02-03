37b5c3902a792a872f40ae25481f1f1a05a1cfd3
tahaa karim <tahaa_libra@hotmail.com>
2015-02-04 04:39:23 +0530

    fixed extention autoloading

--- extentions/autoload.php
+++ extentions/autoload.php
@@ -3,7 +3,7 @@
 spl_autoload_register("autoloadExtentions");
 
 function autoloadExtentions($className) {
-if (file_exists(ROOT.DS.'Extentions'.DS.ucwords($className).DS.ucwords($className).'.Extention.php')) {
-        require_once ROOT.DS.'Extentions'.DS.ucwords($className).DS.ucwords($className).'.Extention.php';
+if (file_exists(ROOT.DS.'extentions'.DS.$className.DS.$className.'.Extention.php')) {
+        require_once ROOT.DS.'extentions'.DS.$className.DS.$className.'.Extention.php';
     }
 }
