a3801f7cdc32edeef982bfaf0753e2cdc5db5376
tahaa karim <tahaa_libra@hotmail.com>
2015-06-27 01:01:53 +0530

    push test

---
+++ resources/views/material/header.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+
+    <!-- Angular Material CSS now available via Google CDN; version 0.10 used here -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/0.10.0/angular-material.min.css">
+    <title> {{title}} </title>
+</head>
+<body>
+
