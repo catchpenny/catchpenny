308c6415737554d112e811a430fde9992035b476
Tahaa Karim <tahaalibra@gmail.com>
2016-01-24 20:57:20 +0530

    welcome page edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,56 +6,59 @@
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
-            html, body {
-                height: 100%;
-            }
+                    html, body {
+                        height: 100%;
+                    }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+                    body {
+                        margin: 0;
+                        padding: 0;
+                        width: 100%;
+                        color: #B0BEC5;
+                        display: table;
+                        font-weight: 100;
+                        font-family: 'Lato';
+                    }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-                background-color: rgba(0, 0, 0, 0.5);
-            }
+                    .container {
+                        text-align: center;
+                        display: table-cell;
+                        vertical-align: middle;
+                        background-color: rgba(0,0,0,0.5);
+                    }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+                    .content {
+                        text-align: center;
+                        display: inline-block;
 
-            .title {
-                font-size: 96px;
-                margin-bottom: 40px;
-            }
+                    }
 
-            .quote {
-                       font-size: 24px;
-                   }
+                    .title {
+                        font-size: 96px;
+                        margin-bottom: 40px;
+                    }
 
-           #background {
-               position: fixed;
-               top: 50%;
-               left: 50%;
-               min-width: 100%;
-               min-height: 100%;
-               width: auto;
-               height: auto;
-               z-index: -100;
-               -webkit-transform: translateX(-50%) translateY(-50%);
-               transform: translateX(-50%) translateY(-50%);
-               background: url(polina.jpg) no-repeat;
-               background-size: cover;
-           }
-</style>
+                    .quote {
+                        font-size: 24px;
+                    }
+
+                    #background {
+                    position: fixed;
+                    top: 50%;
+                    left: 50%;
+                    min-width: 100%;
+                    min-height: 100%;
+                    width: auto;
+                    height: auto;
+                    z-index: -100;
+                    -webkit-transform: translateX(-50%) translateY(-50%);
+                    transform: translateX(-50%) translateY(-50%);
+                    background: url(polina.jpg) no-repeat;
+                    background-size: cover;
+                }
+
+
+                </style>
 
 
 </head>
